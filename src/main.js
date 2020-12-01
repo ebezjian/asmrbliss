a84a029a2207b277c7e1234d24b0bd90ca8eff4b
Ellie Lambert <ebezjian@gmail.com>
2020-12-01 16:47:03 -0500

    add css to src tree

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import './css/styles.css';
+import './css/styles.css';
 
 $(document).on('mouseover', '.container .column', function(){
   $(this).addClass('active').siblings().removeClass('active');
